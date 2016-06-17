0b6017475563aa02163a89a7d000e86af3c681fe
utunomy <utunomy@users.noreply.github.com>
2016-06-17 11:23:44 +0900

    update Program

--- Program.cs
+++ Program.cs
@@ -49,7 +49,7 @@ namespace Roppyakken
         MomijiAndTanzakuAo = 0x2000000000,
         Momiji1 = 0x4000000000,
         Momiji2 = 0x8000000000,
-        YanagiAndAme = 0x10000000000,
+        YanagiAndKaeru = 0x10000000000,
         YanagiAndTsubame = 0x20000000000,
         YanagiAndTanzakuAka = 0x40000000000,
         Yanagi = 0x80000000000,
@@ -62,6 +62,9 @@ namespace Roppyakken
     {
         static void Main(string[] args)
         {
+            // プレイ人数2人。
+            new Manager(2);
+
             Console.ReadKey(true);
         }
     }
@@ -70,10 +73,12 @@ namespace Roppyakken
     /// </summary>
     abstract class CardSet
     {
-        public CardSet(string name)
+        public CardSet(string name, Yamafuda yamafuda)
         {
             Name = name;
+            this.yamafuda = yamafuda;
         }
+        protected readonly Yamafuda yamafuda;
         public string Name { get; set; }
         /// <summary>
         /// Handメンバーはコンソールに手札を表示するための要素である。
@@ -105,6 +110,34 @@ namespace Roppyakken
             hand &= ~card.CardPattern;
         }
         /// <summary>
+        /// Playerクラスの開始状態の構築。呼び出し回数はプレイ人数によって決定する。
+        /// </summary>
+        /// <param name="yamafuda"></param>
+        public void TakeToYamafuda()
+        {
+            // require
+            if (yamafuda.CardStack == null) throw new ArgumentNullException();
+            if (yamafuda.CardStack.Count == 0) throw new Exception("CardStackが空です。");
+
+            Card card = yamafuda.CardPop();
+            AddCard(card);
+        }
+        /// <summary>
+        /// Playerクラスの開始状態の構築。count回数だけCardをPopする。呼び出し回数はプレイ人数によって決定する。
+        /// </summary>
+        /// <param name="yamafuda"></param>
+        /// <param name="count">Popする回数</param>
+        public void TakeToYamafuda(int count)
+        {
+            // requie
+            if (count <= 0) throw new ArgumentOutOfRangeException();
+            if (yamafuda.CardStack == null) throw new ArgumentNullException();
+            if (yamafuda.CardStack.Count == 0) throw new Exception("CardStackが空です。");
+
+            while (count-- != 0)
+                TakeToYamafuda();
+        }
+        /// <summary>
         /// 引数のカードに対し、Manth属性が一致する一枚のCardを返す。
         /// </summary>
         /// <param name="card"></param>
@@ -123,7 +156,7 @@ namespace Roppyakken
     /// </summary>
     class Player : CardSet
     {
-        public Player(string name) : base(name)
+        public Player(string name, Yamafuda yamafuda) : base(name, yamafuda)
         {
         }
         /// <summary>
@@ -143,34 +176,15 @@ namespace Roppyakken
         /// </summary>
         public int Score { get; set; }
         /// <summary>
-        /// Playerクラスの開始状態の構築。呼び出し回数はプレイ人数によって決定する。
-        /// </summary>
-        /// <param name="yamafuda"></param>
-        public void TakeToYamafuda(Yamafuda yamafuda)
-        {
-            Card card = yamafuda.CardPop();
-            AddCard(card);
-        }
-        /// <summary>
-        /// Playerクラスの開始状態の構築。count回数だけCardをPopする。呼び出し回数はプレイ人数によって決定する。
-        /// </summary>
-        /// <param name="yamafuda"></param>
-        /// <param name="count">Popする回数</param>
-        public void TakeToYamafuda(Yamafuda yamafuda, int count)
-        {
-            // requie
-            if (count <= 0) throw new ArgumentOutOfRangeException();
-
-            while (count-- != 0)
-                TakeToYamafuda(yamafuda);
-        }
-        /// <summary>
         /// haguriCardのManth属性が、BafudaクラスのCardsのManth属性と一致しない場合、haguriCardをBafudaのCardsに追加する。
         /// </summary>
         /// <param name="haguriCard">山札から引いたCard。</param>
         /// <param name="bafuda"></param>
         public void PutToBafuda(Card haguriCard, Bafuda bafuda)
         {
+            // requre
+            if (bafuda.Cards == null) throw new ArgumentNullException();
+
             bafuda.AddCard(haguriCard);
         }
         /// <summary>
@@ -200,9 +214,15 @@ namespace Roppyakken
         /// 一致する複数枚のCardから選ばれた一枚のCardと、haguriCardをPlayerクラスのkirifudaに加える。
         /// </summary>
         /// <param name="haguriCard"></param>
+        /// <param name="bafuda"></param>
         /// <param name="getCard">Manth属性において、複数枚の一致するCardのから選ばれたCard</param>
-        public void GetToBafuda(Card haguriCard, Card getCard)
+        public void GetToBafuda(Card haguriCard, Bafuda bafuda, Card getCard)
         {
+            // BafudaクラスのCardsからリムーブする。
+            bafuda.RemoveCard(getCard);
+            // Playerのkirifudaに一致する2枚を追加する。
+            AddKirifuda(haguriCard);
+            AddKirifuda(getCard);
         }
     }
     class Card
@@ -221,7 +241,60 @@ namespace Roppyakken
     {
         public Yamafuda()
         {
-
+            List<Card> cards = new List<Card>();
+            cards.Add(new Card(Pattern.Matsu1,1));
+            cards.Add(new Card(Pattern.Matsu2,1));
+            cards.Add(new Card(Pattern.MatsuAndTanzakuAka,1));
+            cards.Add(new Card(Pattern.MatsuAndTsuru,1));
+            cards.Add(new Card(Pattern.Ume1,2));
+            cards.Add(new Card(Pattern.Ume2,2));
+            cards.Add(new Card(Pattern.UmeAndTanzakuAka,2));
+            cards.Add(new Card(Pattern.UmeAndUguisu,2));
+            cards.Add(new Card(Pattern.Sakura1,3));
+            cards.Add(new Card(Pattern.Sakura2,3));
+            cards.Add(new Card(Pattern.SakuraAndMaku,3));
+            cards.Add(new Card(Pattern.SakuraAndTanzakuAka,3));
+            cards.Add(new Card(Pattern.Fuji1,4));
+            cards.Add(new Card(Pattern.Fuji2,4));
+            cards.Add(new Card(Pattern.FujiAndHototogisu,4));
+            cards.Add(new Card(Pattern.FujiAndTanzakuAka,4));
+            cards.Add(new Card(Pattern.Ayame1,5));
+            cards.Add(new Card(Pattern.Ayame2,5));
+            cards.Add(new Card(Pattern.AyameAndHashi,5));
+            cards.Add(new Card(Pattern.AyameAndTanzakuAka,5));
+            cards.Add(new Card(Pattern.Botan1,6));
+            cards.Add(new Card(Pattern.Botan2,6));
+            cards.Add(new Card(Pattern.BotanAndChou,6));
+            cards.Add(new Card(Pattern.BotanAndTanzakuAo,6));
+            cards.Add(new Card(Pattern.Hagi1,7));
+            cards.Add(new Card(Pattern.Hagi2,7));
+            cards.Add(new Card(Pattern.HagiAndInoshishi,7));
+            cards.Add(new Card(Pattern.HagiAndTanzakuAka,7));
+            cards.Add(new Card(Pattern.Sakura1,8));
+            cards.Add(new Card(Pattern.Sakura2,8));
+            cards.Add(new Card(Pattern.SakuraAndMaku,8));
+            cards.Add(new Card(Pattern.SakuraAndTanzakuAka,8));
+            cards.Add(new Card(Pattern.Kiku1,9));
+            cards.Add(new Card(Pattern.Kiku2,9));
+            cards.Add(new Card(Pattern.KikuAndOchoko,9));
+            cards.Add(new Card(Pattern.KikuAndTanzakuAo,9));
+            cards.Add(new Card(Pattern.Momiji1,10));
+            cards.Add(new Card(Pattern.Momiji2,10));
+            cards.Add(new Card(Pattern.MomijiAndShika,10));
+            cards.Add(new Card(Pattern.MomijiAndTanzakuAo,10));
+            cards.Add(new Card(Pattern.Yanagi,11));
+            cards.Add(new Card(Pattern.YanagiAndKaeru,11));
+            cards.Add(new Card(Pattern.YanagiAndTanzakuAka,11));
+            cards.Add(new Card(Pattern.YanagiAndTsubame,11));
+            cards.Add(new Card(Pattern.Kiri1,12));
+            cards.Add(new Card(Pattern.Kiri2,12));
+            cards.Add(new Card(Pattern.KiriAndHouou,12));
+            cards.Add(new Card(Pattern.KiriAndYellow,12));
+            cards = cards.OrderBy(i => Guid.NewGuid()).ToList();
+            foreach (Card card in cards)
+            {
+                cardStack.Push(card);
+            }
         }
         private readonly Stack<Card> cardStack = new Stack<Card>();
         public Stack<Card> CardStack { get { return cardStack; } }
@@ -236,7 +309,7 @@ namespace Roppyakken
     }
     class Bafuda : CardSet
     {
-        public Bafuda(string name) : base(name)
+        public Bafuda(string name, Yamafuda yamafuda) : base(name, yamafuda)
         {
         }
         /// <summary>
@@ -270,6 +343,40 @@ namespace Roppyakken
                 manthList.Add(card.Manth);
             }
             return manthList;
+        }
+    }
+    class Manager
+    {
+        public Manager(int count)
+        {
+            playCount = count;
+            build(count);
+        }
+        private int playCount;
+        private Yamafuda yamafuda = new Yamafuda();
+        private List<Player> players = new List<Player>();
+        private Bafuda bafuda;
+        private void build(int count)
+        {
+            bafuda = new Bafuda("東1棟1F席1", yamafuda);
+            if (count == 2)
+            {
+                bafuda.TakeToYamafuda(8);
+                players.Add(new Player("太郎",yamafuda));
+                players.Add(new Player("和美",yamafuda));
+                players[0].TakeToYamafuda(8);
+                players[1].TakeToYamafuda(8);
+            }
+            if (count == 3)
+            {
+                bafuda.TakeToYamafuda(6);
+                players.Add(new Player("太郎", yamafuda));
+                players.Add(new Player("和美", yamafuda));
+                players.Add(new Player("紗子", yamafuda));
+                players[0].TakeToYamafuda(7);
+                players[1].TakeToYamafuda(7);
+                players[2].TakeToYamafuda(7);
+            }
         }
     }
 
